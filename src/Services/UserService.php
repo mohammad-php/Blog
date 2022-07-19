3f94d0d4d56911b553525ec84ebd4830a28bf0c7
Moh. Alkharraz <mohammad.alkharraz@7awi.com>
2022-07-19 17:02:31 +0300

    Remove Unused Objects

--- src/Services/UserService.php
+++ src/Services/UserService.php
@@ -3,29 +3,19 @@ declare(strict_types=1);
 
 namespace Blog\Services;
 
-use Blog\Database\DatabaseConfig;
-use Blog\Database\DatabaseConnection;
-use Blog\Database\DatabaseQuery;
 use Blog\Models\UserModel;
 use Blog\Repositories\ArrayEntityManager;
-use Blog\Repositories\User\UserDataMapper;
 use Blog\Repositories\User\UserRepository;
 
 class UserService
 {
 
-    protected $databaseConnection;
-    protected $databaseQuery;
     private ArrayEntityManager $persistence;
     private UserRepository $userRepository;
     private UserModel $userModel;
 
     public function __construct()
     {
-//        $databaseConfig = new DatabaseConfig('localhost','root','root','3306','Blog');
-//        $databaseObj = new DatabaseConnection($databaseConfig);
-//        $this->databaseConnection = $databaseObj->connect();
-//        $this->databaseQuery = new DatabaseQuery($this->databaseConnection);
         $this->userModel = new UserModel();
     }
 
